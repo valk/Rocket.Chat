6dd82b08d0e0658db1ef6433a9a2fa3651ae7ff7
Boris Peterbarg <boris@seekingalpha.com>
2018-06-06 19:04:23 +0300

    Drop TravisCI

--- .travis/namefiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-set -euvo pipefail
-IFS=$'\n\t'
-
-FILENAME="$ROCKET_DEPLOY_DIR/rocket.chat-$ARTIFACT_NAME.tgz";
-
-ln -s /tmp/build/Rocket.Chat.tar.gz "$FILENAME"
-ln -s /tmp/build/Rocket.Chat.tar.gz "$ROCKET_DEPLOY_DIR/rocket.chat-latest.tgz"
