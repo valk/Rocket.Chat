ef251fd2fa483e5fd3bd2aa1d89f37186696ae9c
Boris Peterbarg <boris@seekingalpha.com>
2018-10-15 11:39:30 +0300

    Fix CircleCI config
    The branches and deploy code was RC original instead of ours

--- .circleci/setartname.sh
+++ .circleci/setartname.sh
@@ -3,21 +3,3 @@ if [[ $CIRCLE_TAG ]]; then
 else
   export ARTIFACT_NAME="$(npm run version --silent).$CIRCLE_BUILD_NUM"
 fi
-
-if [[ $CIRCLE_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+-rc\.[0-9]+ ]]; then
-  SNAP_CHANNEL=candidate
-  RC_RELEASE=candidate
-  RC_VERSION=$CIRCLE_TAG
-elif [[ $CIRCLE_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  SNAP_CHANNEL=stable
-  RC_RELEASE=stable
-  RC_VERSION=$CIRCLE_TAG
-else
-  SNAP_CHANNEL=edge
-  RC_RELEASE=develop
-  RC_VERSION=0.66.0-develop
-fi
-
-export SNAP_CHANNEL
-export RC_RELEASE
-export RC_VERSION
