8f15bf2d48e8ed3eabaa3ff214ebabfc61ee006b
Noach Magedman <noach@seekingalpha.com>
2019-03-26 15:48:17 +0200

    PATCH: Launch UPM (User Presence Monitor) conditionally
    Upstream has now implemented this feature differently.
    We should migrate to using their implementation,
    however that requires co-ordination with our Puppet config:
      Specify both USER_PRESENCE_MONITOR and DISABLE_PRESENCE_MONITOR
      so we are implementation independent.  Then drop this patch.
      Finally, drop USER_PRESENCE_MONITOR from Puppet.
    
    # Conflicts:
    #	server/startup/presence.js

--- server/startup/presence.js
+++ server/startup/presence.js
@@ -17,9 +17,7 @@ Meteor.startup(function() {
 
 	UserPresence.start();
 
-	const startMonitor = typeof process.env.DISABLE_PRESENCE_MONITOR === 'undefined'
-		|| !['true', 'yes'].includes(String(process.env.DISABLE_PRESENCE_MONITOR).toLowerCase());
-	if (startMonitor) {
-		UserPresenceMonitor.start();
+	if (process.env.USER_PRESENCE_MONITOR) {
+		return UserPresenceMonitor.start();
 	}
 });
