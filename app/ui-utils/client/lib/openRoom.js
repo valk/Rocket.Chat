ceb6d570e475bc4c9f2c67d669655be1924eefd1
Noach Magedman <noach@seekingalpha.com>
2019-04-30 16:59:09 +0300

    PATCH: [OBSOLETE?]  Store current room name/type in Session
    As near as I can tell, these lines are vestigial.
    They were originally added as part of
      commit 4bbdd6885291f7264726fe0c09c57843b5238302 (in 0.73.1-sa.1)
      "Send mone event directly to SA. Mone events for sent-message, room-opened, etc."
    
    but the lines that use them -- added in that same commit to
      packages/rocketchat-lib/client/methods/sendMessage.js
      packages/rocketchat-ui/client/views/app/room.js
    have since been dropped (but I can't find where).
    
    DEPENDENCY:
    This patch depends on "PATCH: Include room name in room-opened event"

--- app/ui-utils/client/lib/openRoom.js
+++ app/ui-utils/client/lib/openRoom.js
@@ -98,6 +98,8 @@ export const openRoom = function(type, name) {
 		named_room.name = name;
 		fireGlobalEvent('room-opened', named_room);
 
+		Session.set('currentRoomName', named_room.name);
+		Session.set('currentRoomType', named_room.t);
 		Session.set('editRoomTitle', false);
 		setTimeout(() => readMessage.readNow(), 2000);
 		// KonchatNotification.removeRoomNotification(params._id)
