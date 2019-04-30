986cebf3aa2f854cdd444eca36ec1454254e3180
Noach Magedman <noach@seekingalpha.com>
2019-04-30 16:43:42 +0300

    PATCH: [API: user#create/update] Expose preferences attribute
    For some reason this is called "settings" in the web API
    but "preferences" in the object model.
    
    SECURITY CONSIDERATIONS:
    Are we checking that the client is authorized to update
    the user?  It doesn't appear so, but the upstream API
    allows changing far more sensitive properties than just
    the user’s mere preferences.

--- app/api/server/v1/users.js
+++ app/api/server/v1/users.js
@@ -37,6 +37,7 @@ API.v1.addRoute('users.create', { authRequired: true }, {
 			sendWelcomeEmail: Match.Maybe(Boolean),
 			verified: Match.Maybe(Boolean),
 			customFields: Match.Maybe(Object),
+			settings: Match.Maybe(Object),
 		});
 
 		// New change made by pull request #5152
@@ -54,6 +55,9 @@ API.v1.addRoute('users.create', { authRequired: true }, {
 			saveCustomFieldsWithoutValidation(newUserId, this.bodyParams.customFields);
 		}
 
+		if (this.bodyParams.settings) {
+			Users.setPreferences(newUserId, this.bodyParams.settings);
+		}
 
 		if (typeof this.bodyParams.active !== 'undefined') {
 			Meteor.runAsUser(this.userId, () => {
@@ -450,6 +454,7 @@ API.v1.addRoute('users.update', { authRequired: true, twoFactorRequired: true },
 				sendWelcomeEmail: Match.Maybe(Boolean),
 				verified: Match.Maybe(Boolean),
 				customFields: Match.Maybe(Object),
+				settings: Match.Maybe(Object),
 			}),
 		});
 
@@ -459,6 +464,10 @@ API.v1.addRoute('users.update', { authRequired: true, twoFactorRequired: true },
 
 		if (this.bodyParams.data.customFields) {
 			saveCustomFields(this.bodyParams.userId, this.bodyParams.data.customFields);
+		}
+
+		if (this.bodyParams.data.settings) {
+			Users.setPreferences(this.bodyParams.userId, this.bodyParams.data.settings);
 		}
 
 		if (typeof this.bodyParams.data.active !== 'undefined') {
