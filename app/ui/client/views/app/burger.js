74465d7c467075daeba0a3484894ef5144410923
Noach Magedman <noach@seekingalpha.com>
2019-04-30 16:37:53 +0300

    PATCH: Expose Unread Message count
    * Add API for getting a user's count of unread messages.
      Used by sapi.
    * Fires a global message when that count changes

--- app/ui/client/views/app/burger.js
+++ app/ui/client/views/app/burger.js
@@ -4,7 +4,9 @@ import { Layout } from '../../../../ui-utils';
 
 Template.burger.helpers({
 	unread() {
-		return Session.get('unread');
+		const unread = Session.get('unread');
+		window.fireGlobalEvent('unread-messages', { unread_msgs: unread });
+		return unread;
 	},
 	isMenuOpen() {
 		if (Session.equals('isMenuOpen', true)) {
