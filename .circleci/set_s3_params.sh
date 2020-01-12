f99ba4c139d1284c3448fbebbda62c4892d72f6c
Noach Magedman <noach@seekingalpha.com>
2020-01-12 11:20:10 +0200

    PATCH: GitHubCI Configuration
    Tailor the GitHubCI workflow to match our needs.
    
    Note that upstream made a hasty, incomplete transition
    from CircleCI to GithubCI.  Several scripts are still
    located in `.circleci/` and many variables are still named
    `CIRCLECI_*`
    
    Disable some jobs:
      - build-image-pr
      - image-build
    
    We still run:
      - build
      - test
      - deploy
    
    Change the deploy job to:
      - Kick in on our branch names
        - sa_*
        - staging
        - production
      - AWS IAM
        Authenticate as the use `circleci-rocketchat`,
        and then assume the role `rocketchat-deploy`.
        I'm not sure how this approach is more advantageous than just
        granting access rights directly to circleci-rocketchat.
      - S3
        - Upload to our S3 buckets and folder names, not upstream’s.
        - Save a "latest" build, in addition to the versionized one.
        - Non-production/staging branches go in a different folder
          `rocket-chat-update` (vs. `rocket-chat`)
      - Document which scripts export/consume which environment variables

---
+++ .circleci/set_s3_params.sh
@@ -0,0 +1,31 @@
+# Assuming we are credentialed as circleci-rocketchat, assume the role rockechat-deploy.
+# This overwrites our AWS_* credentials.
+# Also, define variables for use in uploading tarballs to S3.
+
+export AWS_DEFAULT_REGION=us-west-2
+
+
+if [[ "$CIRCLE_BRANCH" == "production" ]]; then
+  export AWS_BUCKET=seekingalpha-public-production
+
+  assumed_role_json="$(aws --output json \
+      sts assume-role \
+      --role-arn $PRODUCTION_ROLE \
+      --role-session-name rocketchat-deploy
+  )"
+  assumed_role_variables="$(echo "${assumed_role_json}" | jq -r '
+  "export AWS_SESSION_TOKEN=" + .Credentials.SessionToken + "\n" +
+  "export AWS_ACCESS_KEY_ID=" + .Credentials.AccessKeyId + "\n" +
+  "export AWS_SECRET_ACCESS_KEY=" + .Credentials.SecretAccessKey + "\n"
+  ')"
+  eval "${assumed_role_variables}"
+else
+  export AWS_BUCKET=seekingalpha-public-staging
+fi
+
+
+if [[ "$CIRCLE_BRANCH" == "production" || "$CIRCLE_BRANCH" == "staging" ]]; then
+  export UPLOAD_DIR="rocket-chat"
+else
+  export UPLOAD_DIR="rocket-chat-update"
+fi
