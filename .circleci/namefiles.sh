f4323172f61cea227def54f6e8e68cd96aef9e4e
Noach Magedman <noach@seekingalpha.com>
2020-02-25 16:01:58 +0200

    [MP-608] Github Workflow: Always deploy to a single S3 bucket/folder
    Don't distinguish between staging and production.
    Just build and save it in the same folder.
    All that matters is the RC version and the commit number.
    The Jenkins deploy script can pull from there, regardless
    of target environment.

--- .circleci/namefiles.sh
+++ .circleci/namefiles.sh
@@ -2,7 +2,5 @@
 set -euvo pipefail
 IFS=$'\n\t'
 
-FILENAME="$ROCKET_DEPLOY_DIR/rocket.chat-$ARTIFACT_NAME.tgz";
-
-ln -s /tmp/build/Rocket.Chat.tar.gz "$FILENAME"
+ln -s /tmp/build/Rocket.Chat.tar.gz "$ROCKET_DEPLOY_DIR/rocket.chat-$ARTIFACT_NAME.tgz"
 ln -s /tmp/build/Rocket.Chat.tar.gz "$ROCKET_DEPLOY_DIR/rocket.chat-latest.tgz"
